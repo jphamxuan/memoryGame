afe24a9bb63597165a3c7008ea6a0031b927128f
Josiah Pham <josiah.pham@gmail.com>
2018-06-04 18:14:39 -0700

    3rd commit

--- src/App.js
+++ src/App.js
@@ -11,13 +11,6 @@ class App extends Component {
     friends
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
-
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
@@ -30,8 +23,6 @@ class App extends Component {
             key={friend.id}
             name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
           />
         ))}
       </Wrapper>
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,7 +3,8 @@ import "./Title.css";
 
 const Title = props => (
     <div className="titlebox">
-        <h1>Sesame Street Clicky Game!</h1>
+        <img className="sstitle" src='https://vignette.wikia.nocookie.net/logopedia/images/1/10/Sesame-street-logo_%281%29.png/revision/latest?cb=20161227004438'></img>
+        <h1>Clicky Game!</h1>
         <p>Click on an image to earn points, but don't click on any more than once!</p>
     </div>
 );
